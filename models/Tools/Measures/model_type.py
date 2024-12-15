de9173b92012fed45c948c6de72d6f42f228d775
Michał Pilch <m.pilch2002@gmail.com>
2024-12-15 11:59:14 +0100

    PI-24. Run DETR and VIT training notebooks. Create new notebook for evaluating raw models.

--- models/Tools/Measures/model_type.py
+++ models/Tools/Measures/model_type.py
@@ -6,5 +6,10 @@ class ModelType(Enum):
     Enum describing the type of model. CNN for convolutional neural network model.
     """
     CNN = "cnn"
+    DETR = "detr"
+    VIT = "vit"
+    MethaneMapper = "mm"
+
+    # For legacy compatibility 
     TRANSFORMER = "transformer"
     TRANSFORMER_CLASSIFIER = "transformer_classifier"
