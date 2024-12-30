127b85797f521793a44979e6f48f3e2817a5f9f2
Dave848 <dj300758@student.polsl.pl>
2024-12-31 00:25:48 +0100

    PI-24: Notebook improvements

---
+++ models/Tools/FilesHandler/model_wrapper.py
@@ -0,0 +1,6 @@
+# Due to structure changes after training the best models we had to create files
+# Tools/FilesHandler/model_wrapper.py and Tools/Measures/model_type.py.
+#
+# This action was crucial to keep our models usable
+
+from files_handler.model_wrapper import ModelWrapper
---
+++ models/Tools/Measures/model_type.py
@@ -0,0 +1,6 @@
+# Due to structure changes after training the best models we had to create files
+# Tools/FilesHandler/model_wrapper.py and Tools/Measures/model_type.py.
+#
+# This action was crucial to keep our models usable
+
+from measures.model_type import ModelType
